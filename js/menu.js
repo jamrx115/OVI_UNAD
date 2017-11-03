0b248db78a9b137fcb9a82aba59449e9c6cb194b
jam <jamrx115@gmail.com>
2017-11-02 20:32:36 -0500

    Cambio de estilos

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById('btn-menu');
+var lecturas = document.getElementById('lecturas');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function(){
+  nav.classList.toggle('mostrar');
+});
